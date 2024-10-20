40910b55a2a8d1a74b8a902c2d45d13bc22adc5c
Eꪚꪱꪰꪶ $нꫝ᠔᥆ᤐ <wilkerff00@gmail.com>
2024-10-20 01:36:08 -0400

    Update econ-mensual.js

--- plugins/econ-mensual.js
+++ plugins/econ-mensual.js
@@ -1,19 +1,25 @@
-const DEFAULT_REWARD = 20000; // Recompensa por defecto
+const DEFAULT_REWARD = 20000; // Recompensa mensual por defecto
 const DEFAULT_COOLDOWN = 604800000 * 4; // Tiempo de enfriamiento por defecto (4 semanas)
 
-let handler = async (m, { conn, reward = DEFAULT_REWARD, cooldown = DEFAULT_COOLDOWN }) => {
+let handler = async (m, { conn }) => {
     let user = global.db.data.users[m.sender];
 
-    if (new Date() - user.monthly < cooldown) {
-        throw `⏱️ ¡Ya reclamaste tu regalo mensual! Vuelve en:\n *${msToTime((user.monthly + cooldown) - new Date())}*`;
+    // Verifica si ya ha reclamado la recompensa mensual
+    if (new Date() - user.monthly < DEFAULT_COOLDOWN) {
+        throw `⏱️ ¡Ya reclamaste tu regalo mensual! Vuelve en:\n *${msToTime((user.monthly + DEFAULT_COOLDOWN) - new Date())}*`;
     }
 
-    user.coin += reward; // Añadir recompensa al usuario
-    m.reply(`
+    // Actualiza la cantidad de monedas
+    user.coin += DEFAULT_REWARD;
+
+    // Respuesta al usuario
+    conn.reply(m.chat, `
 \`\`\`🎁 ¡Es hora de tu regalo mensual! 🐢\`\`\`
 
-🪙 *${mssg.money}* : +${reward.toLocaleString()}`);
-    user.monthly = new Date() * 1; // Actualizar la fecha del último reclamo
+🪙 *${mssg.money}* : +${DEFAULT_REWARD.toLocaleString()}`, m);
+
+    // Actualiza la fecha del último reclamo
+    user.monthly = new Date() * 1;
 };
 
 handler.help = ['monthly'];
@@ -23,12 +29,12 @@ handler.command = ['mensual', 'monthly'];
 export default handler;
 
 function msToTime(duration) {
-    var milliseconds = parseInt((duration % 1000) / 100),
+    let milliseconds = parseInt((duration % 1000) / 100),
         seconds = Math.floor((duration / 1000) % 60),
         minutes = Math.floor((duration / (1000 * 60)) % 60),
         hours = Math.floor((duration / (1000 * 60 * 60)) % 24),
         days = Math.floor((duration / (1000 * 60 * 60 * 24)) % 365);
-
+    
     hours = (hours < 10) ? "0" + hours : hours;
     minutes = (minutes < 10) ? "0" + minutes : minutes;
     seconds = (seconds < 10) ? "0" + seconds : seconds;
