d7eac4a860ce821e4cc3c25afe1c7fc77438e3e7
Eꪚꪱꪰꪶ $нꫝ᠔᥆ᤐ <wilkerff00@gmail.com>
2024-10-20 22:36:43 -0400

    Update descargas-play1.js

--- plugins/descargas-play1.js
+++ plugins/descargas-play1.js
@@ -1,71 +1,60 @@
-import fetch from 'node-fetch';
-import yts from 'yt-search';
-import ytdl from 'ytdl-core';
-import axios from 'axios';
-import {youtubedl, youtubedlv2} from '@bochilteam/scraper';
-const handler = async (m, {conn, command, args, text, usedPrefix}) => {
-if (!text) throw `_𝐄𝐬𝐜𝐫𝐢𝐛𝐞 𝐮𝐧𝐚 𝐩𝐞𝐭𝐢𝐜𝐢𝐨́𝐧 𝐥𝐮𝐞𝐠𝐨 𝐝𝐞𝐥 𝐜𝐨𝐦𝐚𝐧𝐝𝐨 𝐞𝐣𝐞𝐦𝐩𝐥𝐨:_ \n*${usedPrefix + command} Billie Eilish - Bellyache*`
-try { 
-const yt_play = await search(args.join(' '))
-const texto1 = `
-╭ׅׄ̇─͓̗̗─ׅ̻ׄ╮۪̇߭⊹߭̇︹ׅ̟ׄ̇︹ׅ۪ׄ̇߭︹ׅ̟ׄ̇⊹۪̇߭︹ׅ̟ׄ̇︹ׅ۪ׄ̇߭︹ׅ̟ׄ̇⊹۪̇߭︹ׅ̟ׄ̇︹ׅ۪ׄ̇߭︹ׅ̟ׄ̇߭︹ׅ۪ׄ̇߭̇⊹
-┟─⬪࣪ꥈ𑁍⃪࣭۪ٜ݊݊݊݊݊໑ٜ࣪𝔻𝔼𝕊ℂ𝔸ℝ𝔾𝔸𝕊໑⃪࣭۪ٜ݊݊݊݊𑁍ꥈ࣪⬪╮
-╭┄─🍂⬪࣪ꥈ𑁍⃪࣭۪ٜ݊݊݊݊݊໑ٜ࣪𝕐𝕦𝕜𝕚_𝕊𝕦𝕠𝕦໑⃪࣭۪ٜ݊݊݊݊𑁍ꥈ࣪⬪╯
-│
-├ ⚘݄𖠵⃕⁖𖥔. _*𝕋í𝕥𝕦𝕝𝕠*_
-├» ${yt_play[0].title}
-├╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌┄
-├ ⚘݄𖠵⃕⁖𖥔. _*ℙ𝕦𝕓𝕝𝕚𝕔𝕒𝕕𝕠*_
-├» ${yt_play[0].ago}
-├╌╌╌╌╌╌╌╌╌╌╌╌╌╌┈
-├ ⚘݄𖠵⃕⁖𖥔. _*𝔻𝕦𝕣𝕒𝕔𝕚ó𝕟*_
-├» ${secondString(yt_play[0].duration.seconds)}
-├╌╌╌╌╌╌╌╌╌╌╌╌┄
-├ ⚘݄𖠵⃕⁖𖥔. _*𝕍𝕚𝕤𝕥𝕒𝕤*_
-├» ${MilesNumber(yt_play[0].views)}
-├╌╌╌╌╌╌╌╌╌╌┄
-├ ⚘݄𖠵⃕⁖𖥔. _*𝔸𝕦𝕥𝕠𝕣(𝕒)*_
-├» ${yt_play[0].author.name}
-├╌╌╌╌╌╌╌╌┈
-├ ⚘݄𖠵⃕⁖𖥔. _*𝔼𝕟𝕝𝕒𝕔𝕖*_
-├» ${yt_play[0].url}
-╰ׁ̻۫─۪۬─۟─۪─۫─۪۬─۟─۪─۟─۪۬─۟─۪─۟─۪۬─۟─۪─۟┄۪۬┄۟┄۪┈۟┈۪`.trim()
+import Starlights from '@StarlightsTeam/Scraper'
+import yts from 'yt-search'
 
-await conn.sendButton(m.chat, wm, texto1, yt_play[0].thumbnail, [['𝕄𝔼ℕ𝕌 ✨', `${usedPrefix}menu`],['🌟 𝔸𝕌𝔻𝕀𝕆',`${usedPrefix}play5 ${yt_play[0].url}`],['🌟 𝕍𝕀𝔻𝔼𝕆',`${usedPrefix}play6 ${yt_play[0].url}`]], null, null, fgif2)
-} catch (e) {
-await conn.reply(m.chat, `*[ ! ] ʜᴜʙᴏ ᴜɴ ᴇʀʀᴏʀ ᴇɴ ᴇʟ ᴄᴏᴍᴀɴᴅᴏ ᴘᴏʀ ғᴀᴠᴏʀ ɪɴᴛᴇɴᴛᴀ ᴍᴀs ᴛᴀʀᴅᴇ..*`, fkontak, m, rcanal)
-console.log(`❗❗ᴇʀʀᴏʀ ${usedPrefix + command} ❗❗`)
-console.log(e)
-handler.limit = 0
-}}
-handler.command = ['play', 'play2', 'play3', 'play4']
-//handler.limit = 3
-handler.register = true 
-handler.group = true
-export default handler;
+let handler = async (m, { conn, text, command }) => {
+  await m.react('🕓')
+  let res = await yts(text)
+  let vid = res.videos[0]
+  let txt = '`乂 Y O U T U B E - P L A Y`\n\n'
+      txt += `	✩   *Título*: ${vid.title}\n`
+      txt += `	✩   *Duración*: ${vid.timestamp}\n`
+      txt += `	✩   *Visitas*: ${formatNumber(vid.views)}\n`
+      txt += `	✩   *Autor*: ${vid.author.name}\n`
+      txt += `	✩   *Publicado*: ${eYear(vid.ago)}\n`
+      txt += `	✩   *Url*: https://youtu.be/${vid.videoId}\n\n`
+      txt += `> *- ↻ El archivo se está enviando, espera un momento, soy lenta. . .*`
 
-async function search(query, options = {}) {
-const search = await yts.search({query, hl: 'es', gl: 'ES', ...options});
-return search.videos;
+  await conn.sendFile(m.chat, vid.thumbnail, 'thumbnail.jpg', txt, m)
+
+  try {
+    let data = await Starlights.ytmp3v2(vid.url) // Puedes cambiar a ytmp4v2 si prefieres video
+    let mimetype = 'audio/mpeg'
+    let file = { url: data.dl_url }
+
+    await conn.sendMessage(m.chat, { audio: file, mimetype, fileName: `${data.title}.mp3` }, { quoted: m })
+    await m.react('✅')
+  } catch {
+    await m.react('✖️')
+  }
 }
 
-function MilesNumber(number) {
-const exp = /(\d)(?=(\d{3})+(?!\d))/g;
-const rep = '$1.';
-const arr = number.toString().split('.');
-arr[0] = arr[0].replace(exp, rep);
-return arr[1] ? arr.join('.') : arr[0];
+handler.help = ['play <búsqueda>']
+handler.tags = ['downloader']
+handler.command = ['ytplay', 'play']
+
+export default handler
+
+function eYear(txt) {
+    if (!txt) {
+        return '×'
+    }
+    // Simplificación de la función para mayor claridad
+    let timeUnits = {
+        "month": "mes",
+        "year": "año",
+        "hour": "hora",
+        "minute": "minuto",
+        "day": "dia"
+    };
+    for (let unit in timeUnits) {
+        if (txt.includes(unit)) {
+            let quantity = txt.replace(`${unit}${txt.includes('ago') ? ' ago' : ''}`, "").trim();
+            return `hace ${quantity} ${timeUnits[unit]}${quantity > 1 ? 's' : ''}`;
+        }
+    }
+    return txt;
 }
 
-function secondString(seconds) {
-seconds = Number(seconds);
-const d = Math.floor(seconds / (3600 * 24));
-const h = Math.floor((seconds % (3600 * 24)) / 3600);
-const m = Math.floor((seconds % 3600) / 60);
-const s = Math.floor(seconds % 60);
-const dDisplay = d > 0 ? d + (d == 1 ? ' día, ' : ' días, ') : '';
-const hDisplay = h > 0 ? h + (h == 1 ? ' hora, ' : ' horas, ') : '';
-const mDisplay = m > 0 ? m + (m == 1 ? ' minuto, ' : ' minutos, ') : '';
-const sDisplay = s > 0 ? s + (s == 1 ? ' segundo' : ' segundos') : '';
-return dDisplay + hDisplay + mDisplay + sDisplay;
+function formatNumber(number) {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
